010df4c6391b16e94c4ee735cd88656ec71465ac
alivething <kaustubhkhedkar02@gmail.com>
2021-12-12 18:24:29 +0530

    CIFAR10 CNN

--- BodyTracker 204/mid_sem_eval.py
+++ BodyTracker 204/mid_sem_eval.py
@@ -1,6 +1,4 @@
 import mediapipe as mp
-import pynput
-from pynput.keyboard import Key, Controller
 import cv2 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -8,29 +6,31 @@ mp_pose = mp.solutions.pose
 
 cap = cv2.VideoCapture(0)
 
-keyboard = Controller()
+
 
 # Initiate pose model
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     
     while cap.isOpened():
         ret, frame = cap.read()
-        
+    # for i in range(0, 5):
+
+        # frame = cv2.imread("C:/Users/kaust/Desktop/College stuff/Book/tg_000"+str(i)+".jpg")
         # Recolor Feed
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-
         # Make Detections
         results = pose.process(image)
-        
+        bg = cv2.imread("C:/Users/kaust/Desktop/College stuff/Black.png")
         
         # Recolor image back to BGR for rendering
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # Pose Detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        mp_drawing.draw_landmarks(bg, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        image = cv2.flip(image, 1)
+        # image = cv2.flip(image, 1)
+        bg = cv2.flip(bg, 1)
 
         #Extracting the coordinates
         lhand = results.pose_landmarks.landmark[15]
@@ -38,36 +38,25 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         lshoulder = results.pose_landmarks.landmark[11]
         rshoulder = results.pose_landmarks.landmark[12]
 
+        # print(lhand)
+
         #Checking if left or right
         if((lhand.x> lshoulder.x)&(rhand.x> rshoulder.x)):
             print("Left")
-            keyboard.release('w')
-            keyboard.release('d')
-            keyboard.press('a')
             
         elif((lhand.x< lshoulder.x)&(rhand.x< rshoulder.x)):
             print("Right")
-            keyboard.release('a')
-            keyboard.release('w')
-            keyboard.press('d')
             
         elif((lhand.y< lshoulder.y)&(rhand.y< rshoulder.y)):
             print("Up")
-            keyboard.release('a')
-            keyboard.release('d')
-            keyboard.press('w')
 
-        elif((lhand.y< lshoulder.y)&(rhand.y< rshoulder.y)):
-            keyboard.release('a')
-            keyboard.release('d')
-            keyboard.release('w')
-
-        if((lhand.y< lshoulder.y)&(rhand.y< rshoulder.y)&(lhand.x< rhand.x)):
+        if((lhand.y> lshoulder.y)&(rhand.y< rshoulder.y)&(lhand.x< rhand.x)):
             print("Lol")
             break
-        # Draw connections
-        
-        cv2.imshow('Raw Webcam Feed', image)
+
+        # Draw connection
+        cv2.imshow('Raw Webcam Feed', bg)
+        # cv2.waitKey(1000)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
--- z.py
+++ z.py
@@ -1,3 +1,13 @@
-import numpy as np
+import matplotlib.pyplot as plt
 
-np.append()
+case = ['Case 1', 'Case 2', 'Case 3']
+latency = [729.93, 729.878, 799.767]
+unpriolatency = [799.767, 799.767, 799.767]
+
+plt.ylabel("Latency (ms)")
+
+plt.plot(case, latency, label = "Prioritized")
+plt.plot(case, unpriolatency, label = "Unprioritized")
+plt.legend()
+plt.grid()
+plt.show()
