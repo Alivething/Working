454a253cfb810f3a22349e7ab04f6021747baa4f
alivething <kaustubhkhedkar02@gmail.com>
2022-04-30 17:13:46 +0530

    Added New Audio files

--- audioextraction/audioext.py
+++ audioextraction/audioext.py
@@ -1,67 +1,67 @@
+# import librosa
 # from pydub import AudioSegment
+# import torch
+# from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+# import time
 
-# mp3_audio = AudioSegment.from_file(r"audioextraction\adio.wav", format="wav")
-# print(len(mp3_audio)/(1000*30))
+# # load model and tokenizer
+# tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+# model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-# counter_audio = 30
-# split_audio = [mp3_audio[:30*1000]]
+# # The base model pretrained and fine-tuned on 960 hours of Librispeech on 16kHz sampled speech audio.
+# # When using the model make sure that your speech input is also sampled at 16Khz.
+
+# # load any audio file of your choice
+# mp3_audio = AudioSegment.from_file(r"adio2.wav", format="wav")
+# collection_of_text = []
+# speechtiming = []
+# starttime = time.time()
 # for i in range(round(len(mp3_audio)/(1000*30))):
-#     split_audio.append(mp3_audio[counter_audio*1000:(counter_audio+30)*1000])
-#     counter_audio += 30
 
-# count = 0
+#     speech, rate = librosa.load(f"{i+1}_audi_file.wav", sr=16000)
 
-# for count, audio_object in enumerate(split_audio):
-#     count += 1
-#     with open(f"{count}_audi_file.wav", 'wb') as out_f:
-#         audio_object.export(out_f, format='wav')
+#     input_values = tokenizer(speech, return_tensors='pt').input_values
+#     # Store logits (non-normalized predictions)
+#     with torch.no_grad():
+#         logits = model(input_values).logits
 
-import librosa
-import torch
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
-import time
+#     # Store predicted id's
+#     predicted_ids = torch.argmax(logits, dim=-1)
+#     # decode the audio to generate text
+#     # Passing the prediction to the tokenzer decode to get the transcription
+#     transcription = tokenizer.batch_decode(predicted_ids)[0]
+#     # transcriptions = tokenizer.decode(predicted_ids[0])
+#     print(transcription)
+#     collection_of_text.append(transcription)
+#     speechtiming.append(round((time.time() - starttime), 2))
 
-# load model and tokenizer
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+# print(collection_of_text)
+# final_complete_speech = ""
+# wordtime = ""
 
-# The base model pretrained and fine-tuned on 960 hours of Librispeech on 16kHz sampled speech audio.
-# When using the model make sure that your speech input is also sampled at 16Khz.
+# # convert batch of text into one complete sentence
+# for i in collection_of_text:  
+#     final_complete_speech += i
 
-# load any audio file of your choice
-collection_of_text = []
-speechtiming = []
-starttime = time.time()
-for i in range(2): #round(len(mp3_audio)/(1000*30))):
+# for i in speechtiming:
+#     wordtime += str(i)
 
-    speech, rate = librosa.load(f"{i+1}_audi_file.wav", sr=16000)
+# file = open("op.txt", "w")
+# file.write(final_complete_speech)
+# file.close()
 
-    input_values = tokenizer(speech, return_tensors='pt').input_values
-    # Store logits (non-normalized predictions)
-    with torch.no_grad():
-        logits = model(input_values).logits
+from summa import keywords
+import yake 
 
-    # Store predicted id's
-    predicted_ids = torch.argmax(logits, dim=-1)
-    # decode the audio to generate text
-    # Passing the prediction to the tokenzer decode to get the transcription
-    transcription = tokenizer.batch_decode(predicted_ids)[0]
-    # transcriptions = tokenizer.decode(predicted_ids[0])
-    print(transcription)
-    collection_of_text.append(transcription)
-    speechtiming.append(round((time.time() - starttime), 2))
+file = open("op.txt")
+final_text = file.read()
 
-print(collection_of_text)
-final_complete_speech = ""
-wordtime = ""
+TR_keywords = keywords.keywords(final_text, scores=True)
+print(TR_keywords[0:10])
 
-# convert batch of text into one complete sentence
-for i in collection_of_text:
-    final_complete_speech += i
+print()
 
-for i in speechtiming:
-    wordtime += str(i)
-
-file = open("op.txt", "w")
-file.write(final_complete_speech+"\n"+wordtime)
-file.close()
+kw_extractor = yake.KeywordExtractor(top=10, stopwords=None, dedupLim=0.3, n=1)
+keywords = kw_extractor.extract_keywords(final_text)
+for kw, v in keywords:
+  print("Keyphrase: ",kw, ": score", v)
