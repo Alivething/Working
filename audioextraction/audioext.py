aa4cc24d059c24825099ae4fc4f5d89449087eab
alivething <kaustubhkhedkar02@gmail.com>
2022-05-06 03:20:44 +0530

    Added link recommender

--- audioextraction/audioext.py
+++ audioextraction/audioext.py
@@ -6,47 +6,47 @@ import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 
 # loading model and tokenizer
-tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+# tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+# model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
-# The base model pretrained and fine-tuned on 960 hours of Librispeech on 16kHz sampled speech audio.
-# When using the model make sure that your speech input is also sampled at 16Khz.
+# # The base model pretrained and fine-tuned on 960 hours of Librispeech on 16kHz sampled speech audio.
+# # When using the model make sure that your speech input is also sampled at 16Khz.
 
-# loading audio file 
-mp3_audio = AudioSegment.from_file(r"adio2.wav", format="wav")
-collection_of_text = []
+# # loading audio file 
+# mp3_audio = AudioSegment.from_file(r"adio2.wav", format="wav")
+# collection_of_text = []
 
-for i in range(round(len(mp3_audio)/(1000*30))):
+# for i in range(round(len(mp3_audio)/(1000*30))):
 
-    speech, rate = librosa.load(f"{i+1}_audi_file.wav", sr=16000)
+#     speech, rate = librosa.load(f"{i+1}_audi_file.wav", sr=16000)
 
-    input_values = tokenizer(speech, return_tensors='pt').input_values
-    # Store logits (non-normalized predictions)
-    with torch.no_grad():
-        logits = model(input_values).logits
+#     input_values = tokenizer(speech, return_tensors='pt').input_values
+#     # Store logits (non-normalized predictions)
+#     with torch.no_grad():
+#         logits = model(input_values).logits
 
-    # Store predicted id's
-    predicted_ids = torch.argmax(logits, dim=-1)
+#     # Store predicted id's
+#     predicted_ids = torch.argmax(logits, dim=-1)
 
-    # decode the audio to generate text
-    # Passing the prediction to the tokenzer decode to get the transcription
-    transcription = tokenizer.batch_decode(predicted_ids)[0]
+#     # decode the audio to generate text
+#     # Passing the prediction to the tokenzer decode to get the transcription
+#     transcription = tokenizer.batch_decode(predicted_ids)[0]
 
-    print(transcription)
-    collection_of_text.append(transcription)
+#     print(transcription)
+#     collection_of_text.append(transcription)
 
-print(collection_of_text)
-final_complete_speech = ""
-wordtime = ""
+# print(collection_of_text)
+# final_complete_speech = ""
+# wordtime = ""
 
-# convert batch of text into one complete sentence
-for i in collection_of_text:  
-    final_complete_speech += i
+# # convert batch of text into one complete sentence
+# for i in collection_of_text:  
+#     final_complete_speech += i
 
-#Writing into a file
-file = open("op.txt", "w")
-file.write(final_complete_speech)
-file.close()
+# #Writing into a file
+# file = open("op.txt", "w")
+# file.write(final_complete_speech)
+# file.close()
 
 #Keyword extraction using Yake
 import yake 
@@ -60,13 +60,18 @@ keywords = kw_extractor.extract_keywords(final_text)
 
 #Printing keywords
 query = ""
+keywords = sorted(keywords, key = lambda v: v in keywords, reverse=True)
 for kw, v in keywords:
   query += kw + " "  
-  print("Keyphrase: ", kw)
+  print(f"Keyphrase: {kw} \t|score: {round(1-v, 3)}")
 
-#Searching google for similar links
+#Searching google for similar news articles
 from googlesearch import search
 
 print() 
-for j in search(query, tld="com", lang = "english", num=20, stop=20, pause=2):
-	print(j)
+file = open("links.txt", "w")
+for j in search(query, tld="com", lang = "english", num=10, stop=20, pause=2):
+    print(j)
+    file.write(j+"\n")
+
+file.close()
